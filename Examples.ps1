c9cadaa702658da2f8407dd8ecb2ce71f2d81a71
Aaron P. Miller <aaron@aaronpmiller.com>
2013-10-03 21:26:43 -0500

    Creating an examples script

---
+++ Examples.ps1
@@ -0,0 +1,9 @@
+Import-Module C:\Users\Aaron\Repos\PoSh-DigitalOcean\DigitalOcean.psm1 -Force
+
+
+Set-DODefaultAuthVariables -clientID 'Your digitalocean clientID' -apiKey 'your digitalocean api key goes here'
+
+[int]$selectedImage = Get-DOImage | Out-GridView -OutputMode Single | % {$_.id}
+[int]$selectedSize = Get-DOSizes | Out-GridView -OutputMode Single | % {$_.id}
+[int]$selectedRegion = Get-DORegions | Out-GridView -OutputMode Single | % {$_.id}
+[int[]]$selectedSSHKeys = Get-DOSSHKey | Out-GridView -OutputMode Multiple | % {$_.id}
